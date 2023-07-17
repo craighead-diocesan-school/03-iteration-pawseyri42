1473808bdf1f972c92743d2d4a7af4087044e8bd
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-17 22:42:04 +0000

    attendees done

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -3,18 +3,18 @@
 // ####################################
 
 function liftOff() {
- let numberCount = prompt (' what number whould you like to count down from? ')
+  let numberCount = prompt(' what number whould you like to count down from? ')
 
   let count = numberCount
- 
+
   while (count > 0) {
     alert(count = count - 1)
   }
-   
+
   alert('Lift Off!')
 }
 
 function make1() {
   // copy and paste your code here when you get to the Make step
-  
+
 }
--- code/2-attendees.js
+++ code/2-attendees.js
@@ -6,18 +6,29 @@ function attendees() {
   let count = 0
   let numOfAttendees = 5
   let attendeeList = ''
-  
+
   while (count < numOfAttendees) {
     let attendee = prompt()
     attendeeList = attendeeList + attendee
     attendeeList = attendeeList + ', '
     count = count + 1
   }
-  
-  alert(attendeeList) 
+
+  alert(attendeeList)
 }
 
 function make2() {
   // copy and paste your code here when you get to the Make step
-  
+  let numberAmount = prompt('How many numbers would you like to add?')
+
+  let count = 0
+  let total = 0
+  while (count < numberAmount) {
+    let number = prompt('Enter one number')
+    number = Number(number)
+    total = total + number
+    count = count + 1
+  }
+
+  alert(' total sum is ' + total)
 }
